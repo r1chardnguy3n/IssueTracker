509df6bbdc62fc4ea55214840dce07efc84be337
Richard Nguyen <reechardc@gmail.com>
2019-12-09 19:00:18 -0800

    Added Web App Project
    created ASP.Net MVC web app with authentication

---
+++ IssueTrackerWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IssueTrackerWebApp.Startup))]
+namespace IssueTrackerWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
